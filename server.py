75695f146f5e50e4e56c002fd2c930924f3e99b5
Ismaiana Cruz De Lima <ismaiana_@hotmail.com>
2023-01-26 00:19:44 -0700

    server updated

--- server.py
+++ server.py
@@ -3,12 +3,13 @@
 
 from flask import Flask, render_template, request, flash, session, redirect
 from model import connect_to_db, db
+import os
 import crud
 
 from jinja2 import StrictUndefined
 
 app = Flask(__name__)
-app.secret_key = ''
+app.secret_key = os.environ.get("secret_key")
 app.jinja_env.undefined = StrictUndefined
 
 connect_to_db(app)
